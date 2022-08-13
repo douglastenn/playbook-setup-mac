6ad4a95c74135dc0c7b817c8fa10c98e2651ac19
Douglas Tenório <106711137+douglastenn@users.noreply.github.com>
2022-08-13 19:25:35 -0300

    Update install-homebrew.sh

--- roles/apps/files/install-homebrew.sh
+++ roles/apps/files/install-homebrew.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"

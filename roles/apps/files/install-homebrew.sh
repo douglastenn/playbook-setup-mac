6ebc101bbae64d38812868d462654366889fb254
Douglas Tenório <106711137+douglastenn@users.noreply.github.com>
2022-08-13 20:42:53 -0300

    fix: homebrew check and eval bin

--- roles/apps/files/install-homebrew.sh
+++ roles/apps/files/install-homebrew.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> ~/.zprofile
+eval $(/opt/homebrew/bin/brew shellenv)
+echo "🍺 Homebrew Installed"

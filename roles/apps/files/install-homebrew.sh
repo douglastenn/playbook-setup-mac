27f5c3c8c3cef35e69fa610705e024119e6530e6
Douglas Tenório <106711137+douglastenn@users.noreply.github.com>
2022-08-13 19:59:25 -0300

    Update install-homebrew.sh

--- roles/apps/files/install-homebrew.sh
+++ roles/apps/files/install-homebrew.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo 'eval \"$(/opt/homebrew/bin/brew shellenv)\"' >> /Users/$(whoami)/.zprofile"
-eval \"$(/opt/homebrew/bin/brew shellenv)\""
+yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
